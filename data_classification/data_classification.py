b0e7b7d7b394cd8e453c8d39c5c1ed56e8f66d9f
GorMing <mingyf2788@126.com>
2018-06-13 00:06:24 +0800

    Update data_classification.py

--- data_classification/data_classification.py
+++ data_classification/data_classification.py
@@ -1,67 +1,41 @@
-import numpy as np
+from __future__ import print_function
 import pandas as pd
-# data = load_exdata('ex1data2.txt');
-# data = np.array(data, np.int64)
+import numpy as np
+from numpy import *
+from sklearn.ensemble import RandomForestClassifier
 
-# x = data[:, (0, 1)].reshape((-1, 2))
-# y = data[:, 2].reshape((-1, 1))
-# m = y.shape[0]
-# print len(x)
-# print len(x[0])
+data = pd.read_csv('train.csv')
+trainx = data.iloc[:, :-1].values
+trainy = data.iloc[:, 128].values
+data = pd.read_csv('test_raw.csv')
+testx = data.iloc[:, :128].values
+testy = np.zeros(len(testx))
 
-data = pd.read_csv('train1.csv')
-x = data.iloc[:, :-1].values
-y = data.iloc[:, 128].values
-m = y.shape[0]
+print (len(testx))
+print (len(trainx))
 
+X_train = trainx
+y_train = trainy
+X_test = testx
+y_test = testy
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(trainx, trainy)
+testy = clf.predict(X_test)
 
-def featureNormalize(X):
-    X_norm = X;
-    mu = np.zeros((1, X.shape[1]))
-    sigma = np.zeros((1, X.shape[1]))
-    for i in range(X.shape[1]):
-        mu[0, i] = np.mean(X[:, i])
-        sigma[0, i] = np.std(X[:, i])
-    X_norm = (X - mu) / sigma
-    return X_norm, mu, sigma
+mu1 = []
+mu2 = []
+for index in range(7910):
+    st = str(index+6000)
+    st1 = "ID_"
+    len1 = 8 - len(st)
+    for i in range(len1):
+        st1 += "0"
+    st1 += st
+    mu1.append(st1)
 
+print (len(mu1))
+for index in range(len(testy)):
+    mu2.append("cls_" + str(testy[index]))
+dataframe = pd.DataFrame({'ID': mu1, 'Pred': mu2})
+dataframe.to_csv("test2.csv", index=False, sep=',')
 
-def computeCost(X, y, theta):
-    m = y.shape[0]
-    print "in compute: "
-    print len(theta)
-    print len(theta[0])
-    C = X.dot(theta) - y
-    J2 = (np.sum((X.dot(theta) - y)**2)) / (2*m)
-    # J2 = (C.T.dot(C)) / (2 * m)
-    return J2
-
-
-def gradientDescent(X, y, theta, alpha, num_iters):
-    m = y.shape[0]
-    J_history = np.zeros((num_iters, 1))
-    print "gradientdescent: "
-    print len(theta)
-    print len(theta[0])
-    for iter in range(num_iters):
-        # theta = theta - (alpha / m) * (X.T.dot(X.dot(theta) - y))
-        theta = theta - (alpha / m) * np.sum(X.T * (X.dot(theta) - y))
-        J_history[iter] = computeCost(X, y, theta)
-    print "gradientdescent: "
-    print len(theta)
-    print len(theta[0])
-    return J_history, theta
-
-
-iterations = 10000
-alpha = 0.01
-m = y.shape[0]
-x, mu, sigma = featureNormalize(x)
-
-X = np.hstack([x, np.ones((x.shape[0], 1))])
-theta = np.zeros((129, 1))
-
-j = computeCost(X, y, theta)
-J_history, theta = gradientDescent(X, y, theta, alpha, iterations)
-
-print('Theta found by gradient descent', theta)
